d5caa6072f267fb070bad2906204114cb31c12b7
naimul.khan <knaimul95@icloud.com>
2020-10-20 22:57:44 -0400

    created 'user-connected', 'user-disconnected', and 'new user' functions between server and scripts

--- scripts.js
+++ scripts.js
@@ -1,46 +1,58 @@
-// invoke the io varibale from the backend node server
-// call the io variable from its local host, declared in server.js
 const socket = io('http://localhost:3000');
 
-// dom manipulation is using javascript to control the web app
-// we are connecting the messageContainer varibale to the messge-container id,
-// messageForm to send-form (again in index.html)
-// meesageInput to message-input 
+// console.log(socket);
+
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-form');
 const messageInput = document.getElementById('message-input'); 
 
-// calls on the server socket, calls the data function and logs the emit function in server.js
 
-//we tested the console log, and it worked :D
-socket.on('chat-message', (data) => 
+const name = prompt('What is your handle?');
+appendMessage('You have joined!'); 
+
+// new user entered -> callback: scripts.js 'new-user'
+socket.emit('new-user', (name)); 
+
+
+socket.on('user-connected', (name) =>
 { 
-    // this console.logs the data passed    
-        // console.log(data);
-
-     // calls the appendMessage function for the data passed and adds it 
-    appendMessage(data); 
+    appendMessage(`${name} connected`)
 })
 
-// a function that uses an event listener
-// calls the addEventListener, which listens for an event, when the submit button is passed, it will perform e(a common syntax for event, e.g. evt, etc.)
+
+socket.on('chat-message', (data) => 
+{
+    appendMessage(`${data.name}: ${data.message}`);
+})
+
+
+socket.on('user-disconnected', (name) => 
+{ 
+    appendMessage(`${name} disconnected`);
+})
+
+
+
 messageForm.addEventListener('submit', (e) =>
 {
-    // below prevents page from refreshing when submit is hit
+
     e.preventDefault();
-    // the function gets the value inside of the form
+
     const message = messageInput.value;
-    // takes in a value and then sends out a value
+
+    appendMessage(`You: ${message}`);
+
     socket.emit('send-chat-message', message);
-    // then we want to clear the input box of whatever we typed in 
+
     messageInput.value = ''; 
 })
 
-// this function will add the message to the last previous message, to create a chat log
 function appendMessage(message) {
+
     const messageElement = document.createElement('div');
-    // adds the message to the new element
+ 
     messageElement.innerText = message; 
-    // appends new element to the message container div
+ 
     messageContainer.append(messageElement);
 }
+
--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-// requires node packet, and then connects to a port
 const io = require('socket.io')(3000); 
 
-// create the first set of instructions
-// this is saying on the connection, run a callback (socket)
+const users = {}; 
+
+
 io.on('connection', (socket) => { 
-    // important to understand that the server is the cook, and when we console log it in the server we see it in the "kitchen" and when we console log in the script, we console log in the "front" of the restaurant 
-    // the below code will show up in our server terminal--notice the "new-user" console is pushed to your server status
-    console.log('new-user');
-    socket.on('send-chat-message', message => 
+    // console.log('new-user');
+
+    socket.on('new-user', (name) => 
     {
-        // broadcast will display to everyone but the sender
-        socket.broadcast.emit('chat-message', message);
-        console.log('server', message);
+        // console.log('server', name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+
+    })
+
+    socket.on('send-chat-message', (message) => 
+    {
+        socket.broadcast.emit('chat-message', {
+            message: message, 
+            name: users[socket.id],
+        });
+
     }); 
 
-    // the server is going to "emit" i.e. on connection, say a chat message--hello lambda
-    // hello-lambda is the data that goes into callback, it is hard coded right now (if correct the console when the index.html page is ran will log the "hello lambda")
+    socket.on('disconnect', () => 
+    { 
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+
+    })
+
     // socket.emit("chat-message", "hello lambda");
 });
 
